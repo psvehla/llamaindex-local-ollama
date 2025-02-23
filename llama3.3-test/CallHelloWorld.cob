27aae819391177cc75ef01d5b575aabe79024d2a
psvehla <psvehla@hotmail.com>
2025-02-23 12:37:45 +1100

    Got it going with Llama3.3.
     Needs 48GB of RAM to work!

---
+++ llama3.3-test/CallHelloWorld.cob
@@ -0,0 +1,8 @@
+IDENTIFICATION DIVISION.
+PROGRAM-ID. CallHelloWorld.
+
+PROCEDURE DIVISION.
+MAIN-PROGRAM.
+    CALL "HelloWorld"
+    DISPLAY "Returned from HelloWorld program."
+    STOP RUN.
--- starter.py
+++ starter.py
@@ -4,7 +4,7 @@ from llama_index.llms.ollama import Ollama
 
 documents = SimpleDirectoryReader("data").load_data()
 Settings.embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-base-en-v1.5")
-Settings.llm = Ollama(model="llama3.2", request_timeout=360.0)
+Settings.llm = Ollama(model="llama3.3", request_timeout=360.0)
 index = VectorStoreIndex.from_documents(documents)
 
 query_engine = index.as_query_engine()
